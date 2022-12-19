dc345d0a73a55ed15c5d5ea08b1b15b5c000e9c6
Gabriel Mendes Rangel <gabrielmendesrangel2@gmail.com>
2022-12-19 12:22:54 -0300

    JavaScript finalizado

--- ex023/totpopulacao.js
+++ ex023/totpopulacao.js
@@ -5,7 +5,15 @@ var somaPopTot = 0;
 for (let i = 0; i < listaNumPopulacoes.length; i++) {
     let numPopulacao = parseInt(listaNumPopulacoes[i].textContent.replace(/\./g, ""));
 
+    if(isNaN(numPopulacao)){
+        listaNumPopulacoes[i].textContent = 0;
+        numPopulacao = 0;
+    };
+
     somaPopTot = somaPopTot + numPopulacao;
 };
 
-totPopulacao.textContent = somaPopTot;
+// totPopulacao.textContent = somaPopTot;
+// totPopulacao.textContent = totPopulacao.textContent.replace(/([0-9]{3})([0-9]{3})([0-9]{3})/g, '$1.$2.$3')
+
+totPopulacao.textContent = somaPopTot.toLocaleString('pt-br');
